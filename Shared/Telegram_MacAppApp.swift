9f0948351520c6b93629a64971d158a7b10121ee
Elechim01 <mikymanny015@gmail.com>
2021-02-22 21:54:41 +0100

    Initial Commit

---
+++ Shared/Telegram_MacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telegram_MacAppApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Telegram_MacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
